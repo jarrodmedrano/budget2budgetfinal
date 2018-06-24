5bd11288d23a1f3e4122fed40f7276ab89f0748c
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-06-24 13:23:07 -0600

    starting server

--- server.js
+++ server.js
@@ -3,5 +3,18 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
-
 const app = express();
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongo DB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello world"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
