80535d6bbd0c3c4574b81085f32484c56f70af04
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-06-30 10:26:29 -0600

    add body parser'

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+//Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 //Load User model
 const User = require("../../models/User");
 
-// @route   GET api/users/test
-// @desc    Tests users route
+// @route   GET api/users/register
+// @desc    Register user
 // @access  Public
-router.get("/test", (req, res) =>
-  res.json({
-    msg: "Users Works"
-  })
-);
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = "Email already exists";
+    }
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const app = express();
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //DB Config
 const db = require("./config/keys").mongoURI;
 
@@ -14,8 +18,6 @@ mongoose
   .connect(db)
   .then(() => console.log("Mongo DB connected"))
   .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello world"));
 
 //Use Routes
 app.use("/api/users", users);
