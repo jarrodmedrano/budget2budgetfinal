bc3a436efe7ee5c394d34dd1bb17aaf7da03dffa
Jarrod Medrano <jmedran@gmail.com>
2019-01-01 11:57:46 -0700

    adding profile default route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -20,6 +20,27 @@ router.get("/test", (req, res) =>
   })
 );
 
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 // @route   POST api/profile
 // @desc    Create or edit user profile
 // @access  Private
