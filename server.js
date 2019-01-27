96ceb491908deaffe05e5956578609c8581c236f
Jarrod Medrano <jmedran@gmail.com>
2019-01-26 17:25:34 -0700

    add switch for prod

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  //like our main.js file
+  app.use(express.static("client/build"));
+  // Express will serve up index.html file
+  // if it does not recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
