b1b71329c9cde109503a422b0ae30bc4729ea53c
Jarrod Medrano <jmedran@gmail.com>
2019-01-30 20:44:50 -0700

    refactoring paycheck form

--- client/src/components/forms/paycheck/Paycheck.js
+++ client/src/components/forms/paycheck/Paycheck.js
@@ -1,51 +1,11 @@
 import React, { Component } from "react";
 import { reduxForm } from "redux-form";
-import PaycheckSuccess from "./PaycheckSuccess";
 import PaycheckForm from "./PaycheckForm";
-import connect from "react-redux/es/connect/connect";
 class Paycheck extends Component {
-  state = { formReview: false };
-
-  renderContent() {
-    if (this.state.showFormReview === true) {
-      return (
-        <div>
-          <PaycheckSuccess />
-        </div>
-      );
-    }
-    return (
-      <div>
-        <PaycheckForm
-          onPaycheckSubmit={() => this.setState({ showFormReview: true })}
-        />
-      </div>
-    );
-  }
-
   render() {
-    return <div>{this.renderContent()}</div>;
+    return <PaycheckForm />;
   }
 }
-
-function mapStateToProps(state) {
-  const {
-    name,
-    income,
-    date,
-    recurring
-  } = state.currentPaychecks.currentPaycheck;
-  return {
-    initialValues: {
-      name,
-      income,
-      date,
-      recurring
-    }
-  };
-}
-
-Paycheck = connect(mapStateToProps)(Paycheck);
 
 export default reduxForm({
   form: "PaycheckForm"
