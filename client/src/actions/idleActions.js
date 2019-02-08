df2d63c913ae81cb3dd75817e652a053c81f8168
Jarrod Medrano <jmedran@gmail.com>
2019-02-07 20:36:07 -0700

    removing logs

--- client/src/actions/idleActions.js
+++ client/src/actions/idleActions.js
@@ -2,14 +2,12 @@ import * as types from "./types";
 import { logoutUser } from "./index";
 
 export const onActive = () => dispatch => {
-  console.log("user is active");
   dispatch({
     type: types.ON_ACTIVE
   });
 };
 
 export const onIdle = () => dispatch => {
-  console.log("user is idle");
   dispatch({
     type: types.ON_IDLE
   });
@@ -18,7 +16,6 @@ export const onIdle = () => dispatch => {
 };
 
 export const changeTimeout = values => dispatch => {
-  console.log("timeout is changed");
   dispatch({
     type: types.CHANGE_IDLE_TIMEOUT,
     payload: values
@@ -26,28 +23,24 @@ export const changeTimeout = values => dispatch => {
 };
 
 export const resetIdleTimer = () => dispatch => {
-  console.log("timeout is reset");
   dispatch({
     type: types.RESET_IDLE_TIMER
   });
 };
 
 export const pauseIdleTimer = () => dispatch => {
-  console.log("timeout is paused");
   dispatch({
     type: types.PAUSE_IDLE_TIMER
   });
 };
 
 export const resumeIdleTimer = () => dispatch => {
-  console.log("timeout is resumed");
   dispatch({
     type: types.RESUME_IDLE_TIMER
   });
 };
 
 export const setElapsed = value => dispatch => {
-  console.log("setting elapsed");
   dispatch({
     type: types.SET_ELAPSED,
     payload: value
@@ -55,7 +48,6 @@ export const setElapsed = value => dispatch => {
 };
 
 export const setRemaining = value => dispatch => {
-  console.log("remaining");
   dispatch({
     type: types.SET_REMAINING,
     payload: value
